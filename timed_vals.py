67bf5153a0d8a5ae2c9dacaa9b4efc71d07f26af
Sachin Myneni <myneni@altair.com>
2020-12-31 01:34:10 -0800

    works for basic case, no DB yet.

--- timed_vals.py
+++ timed_vals.py
@@ -1,4 +1,33 @@
 from datetime import datetime, timedelta
+import random
+from time import sleep
+
+
+def gen_random_val() -> float:
+    # Dummy function
+    """Function to generate dummy values from the "sensor"
+
+    In your program you replace this with the function that
+    returns the real value from the sensor.
+    """
+    return random.random()
+
+def get_condition() -> bool:
+    # Dummy function
+    """ Function to generate the "condition" which we need to check whether
+    the value from the sensor needs to be accumulated or not.
+    Currently this generates a random true/false. 
+    
+    Note that unlike the example you
+    showed me on the paper this true/false condition can switch back
+    and forth in a single day. In your example the true condition persisted
+    over more than 24hrs... We can make that happen too but this is a more
+    general example..
+
+    In your program you replace this with the function that returns the conditon
+    """
+    return random.choice([True,False])
+
 
 def get_filename(check_time=None) -> str:
     # Return the YearMonthDate of today if time > 6AM/check_time
@@ -8,5 +37,22 @@ def get_filename(check_time=None) -> str:
         return (datetime.now() - timedelta(days = 1)).strftime("%Y%m%d")
     else:
         return datetime.now().strftime("%Y%m%d")
+
+
+
+if __name__ == '__main__':
+    myval = {}  # a dictionary of 'date':cumulative_val
+    random.seed(42)
+
+    while True:
+        sleep(1)
+        if get_condition():
+            if get_filename() in myval:
+                myval[get_filename()] += gen_random_val()
+            else:
+                myval[get_filename()] = gen_random_val()
+
+
+
 
 
