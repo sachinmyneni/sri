4dd0eeb9a3c988763add091d009b6a06c1c14501
Sachin Myneni <myneni@altair.com>
2021-01-04 16:41:58 -0800

    extending DB query examples.

--- read_results.py
+++ read_results.py
@@ -3,5 +3,12 @@ import sqlite3
 if __name__ == '__main__':
   con = sqlite3.connect('sensor_data.db')
   cur = con.cursor()
+  cur.execute('SELECT name FROM sqlite_master WHERE type="table" AND name NOT LIKE "sqlite_%"')
+  print(cur.fetchall())
+  cur.execute('SELECT * FROM sqlite_master')
+  print(cur.description)
   cur.execute('select * from sensor_data')
   print(cur.fetchall())
+  cur.execute('select * from sensor_data WHERE date < "20210102"')
+  print(cur.fetchall())
+
